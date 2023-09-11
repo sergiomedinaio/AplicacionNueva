c8ef64429ad47ceb5e8ccec786d5940181737e51
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-09-11 10:49:35 -0300

    feat: add layout on runtime

--- app/src/main/java/ar/edu/davinci/aplicacionnueva/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/aplicacionnueva/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //add layout
+        LinearLayout myLayout = new LinearLayout(this);
+        myLayout.setOrientation(LinearLayout.VERTICAL);
+        //add textView
         TextView myText = new TextView(this);
-        myText.setText("Hello world");
+        myText.setText("Hola a todos");
+        //add textView on Layout
+        myLayout.addView(myText);
+        setContentView(myLayout);
         Log.d("test", "testing app");
     }
 }
